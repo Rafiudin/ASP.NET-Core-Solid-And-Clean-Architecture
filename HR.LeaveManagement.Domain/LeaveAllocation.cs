c4c414969eeb4cd9a853582a0da78c84e28a7ab1
Rafi UD Din <rafi.din@CybersoftNA.com>
2025-04-26 11:41:57 +0500

    create leave management domain

--- HR.LeaveManagement.Domain/LeaveAllocation.cs
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -2,7 +2,7 @@
 
 namespace HR.LeaveManagement.Domain;
 
-public class LeaveAllocation: BaseEntity
+public class LeaveAllocation: BaseEntity 
 {
     public int NumberOfDays { get; set; }
     public LeaveType? LeaveType { get; set; }
